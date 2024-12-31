225fe22098bd2571ca4633ef8397d5d67162f691
Ethan Christensen <ethanlchristensen@outlook.com>
2024-12-31 17:23:01 -0600

    update

--- api/app/settings.py
+++ api/app/settings.py
@@ -129,7 +129,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
